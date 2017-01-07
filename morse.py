59abe8b18dfef492c548544eaa031977fc30457d
MiBatu <invictus@mail.ru>
2017-01-08 01:51:04 +0300

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,5 +1,5 @@
 # Program to convert words and strings to morse code.
-# Creating the Class Morse with .convert method.
+# Creating the Class Morse with .conversion method.
 # As an argument use string with [A-Z,a-z,0-9],[.,:;()'-/?!@] 
 # to convert it to morse code.
 
