8ca23311ada426ff27258ab7f064d8ae76ff8fbf
MiBatu <invictus@mail.ru>
2017-01-07 21:56:38 +0300

    Create morse.py

---
+++ morse.py
@@ -0,0 +1,11 @@
+#Program to convert words and strings to morse code
+
+class Convert():
+  def __init__ (self, text):
+    self.text = text
+    dict = {"A":"_.", "B":"_...", "C":"_._."}
+    
+    result = dict["A"]
+    return result
+    
+print(Convert())
